18619e539f3e7f0c67dff6adcfcfcb5506e7a34d
Sawada Kaku <kaku.sawada.ak@gmail.com>
2018-07-13 15:58:09 +0900

    create python tester

--- utils/tester.py
+++ utils/tester.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-import sys
-import os
-import glob
 import subprocess
 import argparse
 import numpy as np
@@ -14,13 +11,14 @@ seeds = [i for i in range(10)]
 
 def argparser():
     parser = argparse.ArgumentParser(description='compile source and test with some cases')
-    parser.add_argument('source', metavar='source', type=str, nargs=1,
+    parser.add_argument('source', metavar='source', type=str,
                         help='path to source to be compiled')
     return parser
 
 
 def main():
-    args = argparser()
+    parser = argparser()
+    args = parser.parse_args()
     source_path = args.source
     # compile c++ source file
     subprocess.check_output(
@@ -34,13 +32,18 @@ def main():
     for seed in seeds:
         out_data = subprocess.check_output(
             ['java', '-jar', './tester.jar',
-             '-exec', '"{}"'.format(source_path.replace('cpp', 'out')),
+             '-exec', '{}'.format(source_path.replace('cpp', 'out')),
              '-seed', f'{seed}'],
-            timeout=3)
-        score = out_data.split_lines()[-1]
-        score = int(score.split()[-1])
+            timeout=3).decode('utf-8')
+        score = out_data.splitlines()[-1]
+        score = int(float(score.split()[-1]))
         d = {'seed': seed, 'score': score}
-        df.append(d)
+        df = df.append(d, ignore_index=True)
+    df = df.astype(np.uint32)
+    csv_path = source_path.replace('cpp', 'csv').replace('code', 'output')
+    df.to_csv(f'{csv_path}',
+              index=False,
+              )
 
 
 if __name__ == '__main__':
